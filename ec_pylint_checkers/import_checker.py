e6b6b6555ea869c85e5bbef56cc48efb32ebc061
Matt Eastman <matt@meastman.org>
2012-10-16 15:09:26 -0700

    Initial commit

---
+++ ec_pylint_checkers/import_checker.py
@@ -0,0 +1,185 @@
+"""An import checker for Pylint.
+
+Below are descriptions of the messages and excerpts from PEP8
+(http://www.python.org/dev/peps/pep-0008/#imports):
+
+C7001: Imports should be on separate lines:
+
+    Imports should usually be on separate lines, e.g.:
+
+    Yes: import os
+         import sys
+
+    No:  import sys, os
+
+    It's okay to say this though:
+
+    from subprocess import Popen, PIPE
+
+C7002: Imports should be at the top of the file:
+
+    Imports are always put at the top of the file, just after any module
+    comments and docstrings, and before module globals and constants.
+
+C7003: Imports are out of order:
+
+    Imports should be grouped in the following order:
+
+    standard library imports
+    related third party imports
+    local application/library specific imports
+
+    You should put a blank line between each group of imports.
+
+    [This checker assumes that any module that exists in the current directory
+    is part of the local application and all others are standard library or
+    related third party.]
+
+C7004: Imports are out of order:
+
+    [Stricter version of C7003 which also checks that imports are sorted
+    alphabetically within each group.]
+
+C7005: Relative imports for intra-package imports are highly discouraged:
+
+    Relative imports for intra-package imports are highly discouraged. Always
+    use the absolute package path for all imports. Even now that PEP 328 is
+    fully implemented in Python 2.5, its style of explicit relative imports is
+    actively discouraged; absolute imports are more portable and usually more
+    readable.
+"""
+
+import difflib
+import imp
+import os
+import sys
+
+from logilab import astng
+from pylint.interfaces import IASTNGChecker
+from pylint.checkers import BaseChecker
+
+MSGS = {
+    'C7001': ('Imports should be on separate lines',
+              'PEP8: "Imports should usually be on separate lines", '
+              'No: "import sys, os", '
+              'Okay: "from subprocess import Popen, PIPE".'),
+    'C7002': ('Imports should be at the top of the file',
+              'PEP8: "Imports are always put at the top of the file", '
+              'Checks that imports do not happen inside functions.'),
+    'C7003': ('Imports are out of order.\n\n'
+              'Found:\n%s\n\nExpected:\n%s\n\nDiff:\n%s',
+              'PEP8: "Imports should be grouped in the following order: '
+              'standard library imports, related third party imports, '
+              'local application/library specific imports".'),
+    'C7005': ('Relative imports are highly discouraged',
+              'PEP8: "Relative imports for intra-package imports are highly '
+              'discouraged. Always use the absolute package path for all '
+              'imports."'),
+}
+MSGS['C7004'] = (
+    MSGS['C7003'][0],
+    MSGS['C7003'][1] + ' This is a stricter version of C7003 which also '
+    'checks that imports are sorted alphabetically within each group.'
+)
+
+_STDLIB_PREFIX = os.path.realpath(getattr(sys, 'real_prefix', sys.prefix)) + '/'
+_APP_PREFIX = os.path.realpath(os.getcwd()) + '/'
+
+class ImportChecker(BaseChecker):
+    __implements__ = (IASTNGChecker,)
+
+    name = 'ec_imports'
+    msgs = MSGS
+    priority = -2
+    options = ()
+
+    def __init__(self, linter=None):
+        super(ImportChecker, self).__init__(linter)
+        self._imports = []
+
+    def visit_import(self, node):
+        if len(node.names) > 1:
+            self.add_message('C7001', node=node)
+        self._handle_import(node)
+
+    def visit_from(self, node):
+        if node.level:
+            self.add_message('C7005', node=node)
+        self._handle_import(node)
+
+    def _handle_import(self, node):
+        if isinstance(node.parent, astng.Module):
+            self._imports.append(node)
+        else:
+            self.add_message('C7002', node=node)
+
+    def leave_module(self, node):
+        actual = []
+        expected1 = []
+        expected2 = []
+
+        # Determine sort keys for each top-level import.
+        # expected1 (for C7003) contains:
+        #   (group_idx, idx, import_str)
+        # expected2 (for C7004) contains:
+        #   (group_idx, module_name_pieces, import_str)
+        for i, import_node in enumerate(self._imports):
+            if isinstance(import_node, astng.Import):
+                pieces = tuple(import_node.names[0][0].split('.'))
+            else:
+                pieces = (
+                    tuple(node.name.split('.')[:-import_node.level]) +
+                    tuple(filter(None, import_node.modname.split('.'))) +
+                    (import_node.names[0][0],)
+                )
+            import_str = import_node.as_string()
+            actual.append(import_str)
+
+            try:
+                path = imp.find_module(pieces[0])[1]
+            except ImportError:
+                group = 1  # 3rd party
+            else:
+                real_path = os.path.realpath(path)
+                path_pieces = set(real_path.split('/'))
+                if real_path.startswith(_APP_PREFIX):
+                    group = 2  # app-specific
+                elif (real_path.startswith(_STDLIB_PREFIX) and
+                      'site-packages' not in path_pieces and
+                      'dist-packages' not in path_pieces):
+                    group = 0  # stdlib
+                else:
+                    group = 1  # 3rd party
+
+            expected1.append((group, i, import_str))
+            expected2.append((group, pieces, import_str))
+
+        # Complain about C7003 or C7004 (but not both) if appropriate.
+        for msg, expected in [('C7003', expected1), ('C7004', expected2)]:
+            expected.sort()
+            expected_strs = [data[-1] for data in expected]
+            if actual != expected_strs:
+                self.add_message(
+                    msg,
+                    args=(
+                        '\n'.join('  %s' % line for line in actual),
+                        '\n'.join('  %s' % line for line in expected_strs),
+                        ''.join(
+                            difflib.unified_diff(
+                                ['%s\n' % line for line in actual],
+                                ['%s\n' % line for line in expected_strs],
+                                fromfile='actual',
+                                tofile='expected',
+                            )
+                        ),
+                    )
+                )
+                break
+
+        # Reset our state for the next module.
+        self._imports[:] = []
+
+
+def register(linter):
+    """Register this checker."""
+    linter.register_checker(ImportChecker(linter))
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='ec-pylint-checkers',
+    version='0.0.1',
+    description='Additional checkers for pylint',
+    packages=['ec_pylint_checkers'],
+)
